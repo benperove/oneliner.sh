5c74b016cf8241abf0ce7510cf226fe8b8da04c4
Benjamin Perove <benperove@gmail.com>
2018-12-19 19:08:27 -0500

    updates

--- api.py
+++ api.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
 import responder
+import redis
 
-api = responder.API()
+api   = responder.API()
+redis = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+@api.route(before_request=True)
+def prepare_response(req, resp):
+    resp.headers['X-Pizza'] = '42'
 
 @api.route("/{cat}/{name}")
 async def greet_world(req, resp, *, cat, name):
     resp.text = f"{cat}/{name}!"
-
-@api.route("/favicon.ico")
-async def greet_world(req, resp, *, favicon):
-    resp.text = f"favicon.ico"
+    redis.set('mykey', resp.text)
 
 if __name__ == '__main__':
     api.run()
