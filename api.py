db1633b570b4a81b38473c83c41e68b53133dd2b
Benjamin Perove <benperove@gmail.com>
2019-01-06 23:59:43 -0500

    adding color

--- api.py
+++ api.py
@@ -98,6 +98,53 @@ def strip_metadata(result):
 #update result metadata
 
 #colorize results
+def col(text, color=None):
+    c = {
+        #background
+        'b_black'       : '40',
+        'b_blue'        : '44',
+        'b_cyan'        : '46',
+        'b_green'       : '42',
+        'b_light_gray'  : '47',
+        'b_magenta'     : '45',
+        'b_red'         : '41',
+        'b_yellow'      : '43',
+        #foreground
+        'f_black'       : '30',
+        'f_blue'        : '34',
+        'f_brown'       : '33',
+        'f_cyan'        : '36',
+        'f_green'       : '32',
+        'f_light_gray'  : '37',
+        'f_normal'      : '39',
+        'f_purple'      : '35',
+        'f_red'         : '31',
+        #compound
+        'f_dark_gray'   : '1;30',
+        'f_light_blue'  : '1;34',
+        'f_light_cyan'  : '1;36',
+        'f_light_green' : '1;32',
+        'f_light_purple': '1;35',
+        'f_light_red'   : '1;31',
+        'f_white'       : '1;37',
+        'f_yellow'      : '1;33',
+        #special
+        'blink'         : '5',
+        'bold'          : '1',
+        'dim'           : '2',
+        'hidden'        : '8',
+        'reset'         : '0',
+        'reverse'       : '7',
+        'underline'     : '4',
+        #alias
+        'b'             : 'bold',
+        'blk'           : 'blink',
+        'h'             : 'hidden',
+        'rev'           : 'reverse',
+        'rst'           : 'reset',
+        'u'             : 'underline'
+    }
+    return "\033[" + c[color] + 'm' + text + "\033[0m"
 
 #cache - get value
 def cache_read(key):
@@ -116,15 +163,15 @@ def cache_clear():
     redis.flushdb()
 
 #oneliner.sh logo
-def logo(ip = None):
+def logo(ip=None):
     logo = """
-                   _                       _
-                  | (@benperove           | |
-   ___  _ __   ___| |_ _ __   ___ _ __ ___| |__
-  / _ \| '_ \ / _ \ | | '_ \ / _ \ '__/ __| '_ \
- | (_) | | | |  __/ | | | | |  __/ |_ \__ \ | | |
-  \___/|_| |_|\___|_|_|_| |_|\___|_(_)|___/_| |_|
-   """ + ip + """
+                   _                       _      
+                  | (""" + col('@benperove', 'f_light_blue') + """           | |     
+   ___  _ __   ___| |_ _ __   ___ _ __ ___| |__   
+  / _ \| '_ \ / _ \ | | '_ \ / _ \ '__/ __| '_ \  
+ | (_) | | | |  __/ | | | | |  __/ |_ \__ \ | | | 
+  \___/|_| |_|\___|_|_|_| |_|\___|_(_)|___/_| |_| 
+   """ + col(ip, 'f_light_green') + """
 
 """
     return logo
