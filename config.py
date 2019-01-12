98bf1ac39bb0a239495c06fedf2bca73116da721
Benjamin Perove <benperove@gmail.com>
2019-01-12 18:43:51 -0500

    fix redis

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ TOKEN_URL     = 'https://github.com/login/oauth/access_token'
 SCOPE         = 'user,public_repo'
 #redis config
 REDIS_HOST = os.environ['REDIS_HOST']
-REDIS_HOST = 'localhost'
+#REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
 REDIS_DB   = 0
 DATA_DIR   = 'oneliners'
