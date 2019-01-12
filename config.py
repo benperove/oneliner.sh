e6bbad23a6e2b1543d9a21ba69241067bcdb6e23
Benjamin Perove <benperove@gmail.com>
2019-01-12 18:35:25 -0500

    ssl

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 #github login
 SITE          = 'https://api.github.com'
-CALLBACK      = 'http://oneliner.sh/oauth2'
+CALLBACK      = 'https://oneliner.sh/oauth2'
 AUTHORIZE_URL = 'https://github.com/login/oauth/authorize'
 TOKEN_URL     = 'https://github.com/login/oauth/access_token'
 SCOPE         = 'user,public_repo'
