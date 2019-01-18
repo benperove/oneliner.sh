1c93c67854df3cf5cef43819c131c77e5134919a
Benjamin Perove <benperove@gmail.com>
2019-01-17 19:30:59 -0500

    collab bin updates

--- bin/collaborator.sh
+++ bin/collaborator.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ACCESS_TOKEN=$(git config auth.token)
-COLLAB_USERNAME="benperove"
-curl -i -H "Authorization: token $ACCESS_TOKEN" -X PUT -d '' "https://api.github.com/repos/benperove/oneliner.sh/collaborators/$COLLAB_USERNAME"
+#ACCESS_TOKEN=$(git config auth.token)
+COLLAB_USERNAME="$1"
+curl -i -H "Authorization: token $GITHUB_ACCESS_TOKEN" -X PUT -d '' "https://api.github.com/repos/benperove/oneliner.sh/collaborators/$COLLAB_USERNAME"
