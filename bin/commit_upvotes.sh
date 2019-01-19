8d5c0ebb246e15d1bce361525e3613fbcacd8d90
Benjamin Perove <benperove@gmail.com>
2019-01-18 20:18:41 -0500

    unnecessary

--- bin/commit_upvotes.sh
+++ bin/commit_upvotes.sh
@@ -4,4 +4,5 @@ git branch --set-upstream-to=origin/upvotes
 git pull origin upvotes --no-edit
 git add oneliners/
 git commit -m 'upvotes counted'
-git push --set-upstream origin upvotes
+#git push --set-upstream origin upvotes
+git push
