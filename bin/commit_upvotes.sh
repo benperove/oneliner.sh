83d1215fe35ed2a30c7a9c6680d3cfe34effeabf
Benjamin Perove <benperove@gmail.com>
2019-01-18 20:11:42 -0500

    changes

--- bin/commit_upvotes.sh
+++ bin/commit_upvotes.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#git checkout -b upvotes
 git pull origin upvotes --no-edit
+git checkout -b upvotes
 git branch --set-upstream-to=origin/upvotes
 git add oneliners/
 git commit -m 'upvotes counted'
